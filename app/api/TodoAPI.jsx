ee0ad3bc82097236a66d1a09dacd57be7c0ca0a6
Matthew Heath <matthew.heath@hotmail.co.uk>
2017-06-21 11:32:51 +0100

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,11 +18,31 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : []
-    //
-    // if ($.isArray (todos)) {
-    //   return todos;
-    // } else {
-    //   return [];
-    // }
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    //Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    }); //'filter' is a built in array method that allows you to filter an existing array and it requires a callback
+
+    //Filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+
+    //Sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed){
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    return filteredTodos;
   }
 };
